92ea968667efd5a717138deae4563289b98f746d
Enzo Sandré <atelier@enzosandre.fr>
2023-11-24 16:38:40 +0100

    Refacto

--- test/rover.test.ts
+++ test/rover.test.ts
@@ -1,25 +1,18 @@
 import {Rover} from "../src/Rover";
 
 describe('Un Rover peut avancer', () => {
-    test('ETANT DONNE un Rover atterrissant en (0, 0)' +
+    test.each([
+        [0, 0],
+        [1, 1]
+    ])('ETANT DONNE un Rover atterrissant en (latitude, longitude) ' +
         'QUAND on le fait avancer ' +
-        'ALORS il passe en (0, 1)', () => {
-        let rover = new Rover(0, 0)
+        'ALORS sa latitude augmente de 1 ' +
+        'ET sa longitude reste la même', (latitude, longitude) => {
+        let rover = new Rover(latitude, longitude)
 
         rover = rover.Avancer();
 
-        expect(rover.Position.Latitude).toBe(1);
-        expect(rover.Position.Longitude).toBe(0);
-    });
-
-    test('ETANT DONNE un Rover atterrissant en (1, 1)' +
-        'QUAND on le fait avancer ' +
-        'ALORS il passe en (1, 2)', () => {
-        let rover = new Rover(1, 1)
-
-        rover = rover.Avancer();
-
-        expect(rover.Position.Latitude).toBe(2);
-        expect(rover.Position.Longitude).toBe(1);
+        expect(rover.Position.Latitude).toBe(latitude + 1);
+        expect(rover.Position.Longitude).toBe(longitude);
     });
 });
